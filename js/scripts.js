4a891b763de3b0cc811a6076d7cb043c8ab61762
MikeMurrey <108901311+MikeMurrey@users.noreply.github.com>
2022-07-15 11:37:16 -0600

    Update scripts.js
    Added Hello World alert and defined favoriteFood variable and added related document.write

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+alert('Hello World');
+
 let myName = 'Bob';
 document.write(myName);
 
@@ -11,3 +13,6 @@ let size=100;
 let doubleSize=size*2;
 let minSize=(doubleSize*2)-(size/2);
 document.write(minSize);
+
+let favoriteFood='Green Chili';
+document.write(favoriteFood);
