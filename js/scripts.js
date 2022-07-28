69b75f37f8442852b9086638631a7be6362ca848
MikeMurrey <108901311+MikeMurrey@users.noreply.github.com>
2022-07-28 14:07:06 -0600

    Update scripts.js
    Wrap pokemonList in IIFE and create getAll and add functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
 /*jshint esversion: 6*/
 
-let pokemonList = [
-  {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
-  {name: 'Ivysaur', height: 1, type: ['Grass', 'Poison']},
-  {name: 'Venusaur', height: 2, type: ['Grass', 'Poison']},
-  {name: 'Charmander', height: 0.6, type: ['Fire']},
-  {name: 'Charmeleon', height: 1.1, type: ['Fire']},
-  {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
-  {name: 'Squirtle', height: 0.5, type: ['Water']},
-  {name: 'Wartortle', height: 1, type: ['Water']},
-  {name: 'Blastoise', height: 1.6, type: ['Water']}
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
+    {name: 'Ivysaur', height: 1, type: ['Grass', 'Poison']},
+    {name: 'Venusaur', height: 2, type: ['Grass', 'Poison']},
+    {name: 'Charmander', height: 0.6, type: ['Fire']},
+    {name: 'Charmeleon', height: 1.1, type: ['Fire']},
+    {name: 'Charizard', height: 1.7, type: ['Fire', 'Flying']},
+    {name: 'Squirtle', height: 0.5, type: ['Water']},
+    {name: 'Wartortle', height: 1, type: ['Water']},
+    {name: 'Blastoise', height: 1.6, type: ['Water']}
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
 
 // loop will iterate over the pokemonList array and write each pokemons' name and height
 // if/else used to write special text for pokemon with a height greater than/equal to 2 or less than/equal to 0.5
