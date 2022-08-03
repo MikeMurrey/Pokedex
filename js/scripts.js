ee9bef10bb5faa761dd73ae2e92686037388de10
MikeMurrey <108901311+MikeMurrey@users.noreply.github.com>
2022-08-03 15:09:45 -0600

    Update scripts.js
    added detailsUrl as a key in the add function

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,8 @@ let pokemonRepository = (function () {
   function add(pokemon) {
     if (
       typeof pokemon === 'object' &&
-      'name' in pokemon
+      'name' in pokemon &&
+      'detailsUrl' in pokemon
     ) {
       pokemonList.push(pokemon);
     } else {
